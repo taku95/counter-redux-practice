9fd1627edcca269ebfbd5ffd04c8b3c666bbd44c
taku95 <t.y.npng1995@gmail.com>
2023-06-07 19:03:09 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Heyyyyyy
+      </Button>
+    </div>
+  );
+}
+
+export default App;
