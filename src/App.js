9599de2cf413bfbe4c5ac9ee3e4c763669d5ce6b
taku95 <t.y.npng1995@gmail.com>
2023-06-08 19:15:09 +0900

    Createcounter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
-import Button from "@mui/material/Button";
+import Counter from "./Counter";
+import { Provider } from "react-redux";
+import store from "./store";
+import { Box } from "@mui/system";
 
 function App() {
   return (
-    <div>
-      <Button variant="contained" color="primary">
-        Heyyyyyy
-      </Button>
-    </div>
+    <Provider store={store}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+        }}
+      >
+        <Counter />
+      </Box>
+    </Provider>
   );
 }
 
---
+++ src/Counter.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "./counterSlice";
+import Button from "@mui/material/Button";
+
+const Counter = () => {
+  const count = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <Button
+        variant="contained"
+        onClick={() => dispatch(increment())}
+        sx={{ margin: "0 8px" }}
+      >
+        +
+      </Button>
+      <span>{count}</span>
+      <Button
+        variant="contained"
+        onClick={() => dispatch(decrement())}
+        sx={{ margin: "0 8px" }}
+      >
+        -
+      </Button>
+    </div>
+  );
+};
+
+export default Counter;
